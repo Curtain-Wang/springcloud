55cb1c02fd16a3cfd459900254bdcff97d361960
Curtain-Wang <wangqt1997@qq.com>
2021-03-23 09:44:43 +0800

    搭建eureka集群

---
+++ springcloud-eureka-7003/src/main/java/com/curtain/springcloud/EurekaServer_7003.java
@@ -0,0 +1,14 @@
+package com.curtain.springcloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+//启动之后访问localhost:7003
+@SpringBootApplication
+@EnableEurekaServer //开始Eureka服务，能让别人注册进来
+public class EurekaServer_7003 {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServer_7003.class);
+    }
+}
--- springcloud-provider-dept-8001/src/main/java/com/curtain/springcloud/DepeProvider_8001.java
+++ springcloud-provider-dept-8001/src/main/java/com/curtain/springcloud/DepeProvider_8001.java
@@ -2,9 +2,13 @@ package com.curtain.springcloud;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 //启动类
 @SpringBootApplication
+@EnableEurekaClient //在服务启动后，自动注册到eureka中!
+@EnableDiscoveryClient //服务发现
 public class DepeProvider_8001 {
     public static void main(String[] args) {
         SpringApplication.run(DepeProvider_8001.class, args);
